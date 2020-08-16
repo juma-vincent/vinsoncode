b73c192f9229aff0b91a056aff5fcf81f0f315f2
juma-vincent <vinsoncode@gmail.com>
2020-08-16 16:09:12 +0300

    Updates

---
+++ precache-manifest.f425baf1774875d7b55683809bca13f5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38f5a61fb2ebfbee69245ad7347c8aea",
+    "url": "/vinsoncode/index.html"
+  },
+  {
+    "revision": "5302b856b5ea203021bf",
+    "url": "/vinsoncode/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "ec21a13040e265f342d7",
+    "url": "/vinsoncode/static/css/main.bdc36011.chunk.css"
+  },
+  {
+    "revision": "5302b856b5ea203021bf",
+    "url": "/vinsoncode/static/js/2.182bb9b0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/vinsoncode/static/js/2.182bb9b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec21a13040e265f342d7",
+    "url": "/vinsoncode/static/js/main.7acdc1f8.chunk.js"
+  },
+  {
+    "revision": "d5596112f7c964b90e95",
+    "url": "/vinsoncode/static/js/runtime-main.292abb1d.js"
+  }
+]);
