ea5c5c110dc00bfd78a8065d1001ebc1536efe84
juma-vincent <vinsoncode@gmail.com>
2021-01-25 17:10:27 +0300

    added a birthday wish

--- client/src/pages/homepage/homepage.jsx
+++ client/src/pages/homepage/homepage.jsx
@@ -18,14 +18,17 @@ const Homepage = () => {
           <div className="content">
             <div className="image-container"></div>
             <div className="description">
-              Hi, my name is Vincent Juma, a full stack web developer proficient
+            Wishing myself the happiest birthday ever! 
+            Thank You, <a id='github-link' href="https://github.com/juma-vincent">God</a>, for letting me have a lovely family 
+            and celebrate my life with them! I am truly blessed!
+              {/* Hi, my name is Vincent Juma, a full stack web developer proficient
                in Node/Express, React Js, Redux and MongoDB. I have
               2 years of experience writing production level code with some of the popular Javascript 
               Libraries. I take pride in creating solutions to problems faced by clients and 
               I ensure any client I serve gets the best from me. I'm a curious persion, I love
               learning new and challenging things, I enjoy solving problems. Am also an active contributor
               to open source projects on <a id='github-link' href="https://github.com/juma-vincent">github</a>.
-              Feel free to reach out to me, via the contact details below. 
+              Feel free to reach out to me, via the contact details below.  */}
             </div>
           </div>
         </div>
