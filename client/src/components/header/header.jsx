83660f561ae8008cbda7f6747ebdca014f3e352a
juma-vincent <vinsoncode@gmail.com>
2022-04-28 18:03:13 +0300

    a few edits

--- client/src/components/header/header.jsx
+++ client/src/components/header/header.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { HeaderContainer } from "./header.styled"; 
-import "./header.css";
+import "./header.scss";
 
 const Header = () => {
   return (
