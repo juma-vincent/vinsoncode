5006b1db759aba38f708aadc97f0fe42041bbe4a
juma-vincent <vinsoncode@gmail.com>
2022-04-28 17:03:31 +0300

    removed tachyons

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,6 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import "tachyons";
 import store from "./redux/store";
 
 ReactDOM.render(
